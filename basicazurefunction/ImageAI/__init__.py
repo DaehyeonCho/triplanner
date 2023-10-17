07d3f5e8325d1d48df83cb80e7ecea73b56f5444
Daehyeon Cho <69081057+DaehyeonCho@users.noreply.github.com>
2023-10-17 13:40:25 +0900

    Add files via upload

---
+++ basicazurefunction/ImageAI/__init__.py
@@ -0,0 +1,38 @@
+import logging
+import openai
+import azure.functions as func
+
+secret_key = "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"
+
+# 생성 샘플
+# {"prompt" : "Goku running though a field", "n" : 1, "size" : "1024x1024"}
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # openai secret key 인증
+    openai.api_key = secret_key
+
+    # HTTP에서 변수 받기
+    req_body = req.get_json()
+
+    # openai api 부르기
+    output = openai.Image.create(
+        prompt = req_body["prompt"],
+        n = req_body["n"],
+        size = req_body["size"]
+    )
+
+    # 응답 포맷
+    output_text = output["data"][0]["url"]
+
+    # 응답 제공
+    return func.HttpResponse(output_text, status_code=200)
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
---
+++ basicazurefunction/completionAPI/__init__.py
@@ -0,0 +1,36 @@
+import logging
+import openai
+import azure.functions as func
+
+secret_key = "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"
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # openai secret key 인증
+    openai.api_key = secret_key
+
+    # HTTP에서 변수 받기
+    req_body = req.get_json()
+
+    # openai api 부르기
+    output = openai.ChatCompletion.create(
+        model = req_body["model"],
+        messages = req_body["messages"],
+        max_tokens = req_body["max_tokens"],
+        temperature = req_body["temperature"]
+    )
+
+    # 응답 포맷
+    output_text = output.choices[0].message["content"].strip()
+
+    # 응답 제공
+    return func.HttpResponse(output_text, status_code=200)
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
